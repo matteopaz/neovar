037f6e0816d29ad20351c08ced298447ea8006a6
Metteo Paz <mpaz@ipac.caltech.edu>
2024-06-16 22:15:35 -0700

    Clustering progress

--- clustering/func.py
+++ clustering/func.py
@@ -5,27 +5,31 @@ import hpgeom
 import pyarrow.compute
 import pyarrow.dataset
 
-
 PARTITION_K = 5
 PARTITION_NSIDE = hpgeom.order_to_nside(order=PARTITION_K)
 
 # [TODO]
 # Set MAX_K to the max order stored in the new files.
-MAX_K = 10
+MAX_K = 13
 # Choose ITER_K such that one pixel plus the margin fits comfortably in memory (all rows, all years).
-ITER_K = 8
+ITER_K = 6
 # Choose MARGIN_K such that the pixels are as small as possible while still including everything
 # that needs to be considered when clustering an ITER_K pixel.
 # To see how big a pixel is, you may want to use an hpgeom method like max_pixel_radius or nside_to_pixel_area.
-MARGIN_K = 10
+MARGIN_K = 12
 
 
 def run():
-    # I would parallelize over partitions, but just to show the basics with a for-loop ...
-    year_datasets = []  # [TODO] fill in
+    base_path = "/stage/irsa-data-parquetlinks/links-tmp/neowiser/healpix_k5/"
+    year_path = "year<N>_skinny/neowiser-healpix_k5-year<N>_skinny.parquet/_metadata"
+    neowise_path = lambda year: base_path + year_path.replace("<N>", str(year))
+    year_datasets = [
+    pyarrow.dataset.parquet_dataset(neowise_path(year), partitioning="hive") for year in range(1, 11)
+    ]
     neowise_ds = pyarrow.dataset.dataset(year_datasets)
-    for partition_k_pixel_id in range(hpgeom.nside_to_npixel(PARTITION_NSIDE)):  # [TODO] check
-        find_clusters_one_partition(partition_k_pixel_id, neowise_ds)
+    # for partition_k_pixel_id in range(hpgeom.nside_to_npixel(PARTITION_NSIDE)):  # [TODO] check
+    partition_k_pixel_id = 10936
+    find_clusters_one_partition(partition_k_pixel_id, neowise_ds)
 
 
 def find_clusters_one_partition(partition_k_pixel_id: int, neowise_ds: pyarrow.dataset.Dataset):
@@ -36,7 +40,7 @@ def find_clusters_one_partition(partition_k_pixel_id: int, neowise_ds: pyarrow.d
     for iter_k_pixel_id in iter_k_pixel_ids:
         filters = construct_filters(partition_k_pixel_id, iter_k_pixel_id)
         pixel_tbl = neowise_ds.to_table(filter=filters)  # pyarrow Table. could convert to pandas, astropy, etc.
-        your_clustering_code(pixel_tbl)
+        cluster(pixel_tbl)
 
 
 def change_k(*, pix: int, pix_k: int, new_k: int, return_field=False) -> int | list[int] | pyarrow.Field:
@@ -74,6 +78,8 @@ def construct_filters(partition_k_pixel_id: int, iter_k_pixel_id: int):
     # Construct pyarrow fields for each column. We need to use pyarrow.compute for all math functions.
     partition_k_field = pyarrow.compute.field(f"healpix_k{PARTITION_K}")
     max_k_field = pyarrow.compute.field(f"healpix_k{MAX_K}")
+    snr_field = pyarrow.compute.field("w1snr")
+
     # Pixel IDs at ITER_K and MARGIN_K are not stored in the catalog, but the values can be
     # calculated from the max_k_field on the fly and used in the filters.
     iter_k_field = change_k(pix=max_k_field, pix_k=MAX_K, new_k=ITER_K, return_field=True)
@@ -87,8 +93,15 @@ def construct_filters(partition_k_pixel_id: int, iter_k_pixel_id: int):
         partition_k_pixel_id, iter_k_pixel_id, partition_k_field, margin_k_field
     )
 
+    ## QUALITY FILTERS ##
+
+    snr_cutoff = snr_field > 4.0
+
+    quality_filter = snr_cutoff
+
+
     # Construct the full filter for rows that are in the pixel or the margin.
-    filters = pixel_filter | margin_filter
+    filters = pixel_filter | margin_filter | quality_filter
     return filters
 
 
@@ -119,7 +132,6 @@ def _construct_margin_filter(
     return margin_filter
 
 
-def your_clustering_code(pixel_tbl):
-    # run your clustering code
-    # you'll probably want to write the results to disk rather than returning them
+def cluster(pixel_tbl):
+    # Write to disk here
     pass

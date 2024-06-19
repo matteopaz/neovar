73c3eba015590c192b2aa067af3a75fe99385015
Matteo Paz <matteo@Matteos-MacBook-Pro.local>
2024-06-19 14:11:34 -0700

    Started bash script

---
+++ clustering/cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=neowise-clustering-mpaz
+#SBATCH --ntasks=4
+#SBATCH --exclusive
+
+python3 init_clustering.py
+
+for partition_id in {0..3}
+do
+    srun --ntasks=1 sleep 1 & python3 print_test.py --partition_id $partition_id &
